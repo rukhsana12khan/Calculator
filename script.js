687a6a653374cb21961d9d730ff1210bc4288e80
rukhsana <99307832+rukhsana12khan@users.noreply.github.com>
2023-02-24 15:44:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let scrn=document.getElementById('screen');
+ 
+
+function input(x){
+    scrn.value=scrn.value+x
+}
+
+function allclear(){
+    scrn.value =' ';
+}
+
+function delet(){
+    scrn.value=scrn.value.slice(0,-1);
+}
+
+function calc(){
+    try{
+        scrn.value=eval(scrn.value);
+    }catch{
+        scrn.value='err'
+    }
+   
+}
